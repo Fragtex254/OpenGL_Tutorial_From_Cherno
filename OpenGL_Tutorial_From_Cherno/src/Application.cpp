60c573bb6587224d49f546133db38715d8a81bb9
CaJiHa <2189665826@qq.com>
2022-12-09 22:18:44 +0800

    Episode-10

--- OpenGL_Tutorial_From_Cherno/src/Application.cpp
+++ OpenGL_Tutorial_From_Cherno/src/Application.cpp
@@ -12,6 +12,41 @@
 * https://glew.sourceforge.net/
 */
 
+#pragma region Debug #define
+#define ASSERT(x) if(!(x)) __debugbreak();
+//use this back-slant to keep the code stiil in one #define
+#define GLCall(x) GLClearError();\
+	x;\
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+//"#x" is the return the function name as a const char* array
+//"__FILE__" is the current file where the breakpoint be hitted
+//"__LINE__" is the current line where the breakpoint be hitted
+
+/*
+* after we have this define 
+* we can wrap our function into the GLCALL(our_Function) to check whether we have made sth wrong
+*/
+
+#pragma endregion
+
+
+#pragma region Debug function
+void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << " )" << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+#pragma endregion
+
 #pragma region Shader Function
 struct ShaderProgramSources
 {
