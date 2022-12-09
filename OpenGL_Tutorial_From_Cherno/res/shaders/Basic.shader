65875633d4ba2e309120fa7c3e276f7fa2fe228a
CaJiHa <2189665826@qq.com>
2022-12-09 21:13:32 +0800

    Episode-8

---
+++ OpenGL_Tutorial_From_Cherno/res/shaders/Basic.shader
@@ -0,0 +1,21 @@
+#shader vertex
+#version 330 core
+
+layout(location = 0) in vec4 position;
+//if we input vec2 this will implicit conversion to vec4
+
+void main()
+{
+	gl_Position = position;
+};
+
+#shader fragment
+#version 330 core
+
+layout(location = 0) out vec4 color;
+
+
+void main()
+{
+	color = vec4(0.2, 0.3, 0.8, 1.0);
+};
--- OpenGL_Tutorial_From_Cherno/src/Application.cpp
+++ OpenGL_Tutorial_From_Cherno/src/Application.cpp
@@ -1,6 +1,9 @@
 #include <GL\glew.h>
 #include <GLFW/glfw3.h>
 #include<iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 /*
 * all the learning resources link here:
@@ -8,6 +11,94 @@
 * https://learnopengl-cn.github.io/
 * https://glew.sourceforge.net/
 */
+
+#pragma region Shader Function
+struct ShaderProgramSources
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+
+};
+
+static ShaderProgramSources ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    //This enum class just like a classifier
+    enum class ShaderType 
+    {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+
+    /*    
+    *Read the all lines in the file and find the flag "#shader" and then start to pick line up
+    *Set the classifier's state and put the line into two shader string stream
+    *And use the constructor to return a "ShaderProgramSources" struct
+    */
+
+    while (std::getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+        }
+        else {
+            ss[(int)type] << line << "\n";
+        }
+    }
+    return { ss[0].str(),ss[1].str() };
+}
+
+unsigned int compileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    //check the state of shader if sth wrong will print log in the console
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compiled " <<
+            (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);//don't forget to delete shader in this case
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int  CreataShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    //link the shader with the program
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    //delete the shader
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    //and return the "V&Fshadered" program id
+    return program;
+}
+#pragma endregion
 
 
 int main(void)
@@ -55,6 +146,14 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2*sizeof(float),nullptr);
 
+    ShaderProgramSources source = ParseShader("res/shaders/Basic.shader");
+    std::cout << "Vertex Shader" << std::endl;
+    std::cout << source.VertexSource << std::endl;
+    std::cout << "Fragment Shader" << std::endl;
+    std::cout << source.FragmentSource << std::endl;
+
+    unsigned int shader = CreataShader(source.VertexSource, source.FragmentSource);
+    glUseProgram(shader);
 
 
     #pragma endregion
@@ -79,6 +178,7 @@ int main(void)
 
 
     #pragma region terminal
+    glDeleteProgram(shader);
     glfwTerminate();
     #pragma endregion
 
