4e6deedbe27e4178a59e4c06af32a5c4b13ea635
CaJiHa <2189665826@qq.com>
2022-12-13 16:51:25 +0800

    Episode-21

--- OpenGL_Tutorial_From_Cherno/src/Application.cpp
+++ OpenGL_Tutorial_From_Cherno/src/Application.cpp
@@ -94,12 +94,16 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/kazuha.png");
         texture.Bind();
